c2dc20b3ba9a32d837d020ef3b79adb1c86823ae
Tiffany Hrabusa <tiffany_hrabusa@gmail.com>
2011-09-01 08:06:00 -0700

    Fixing deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-git pull && \
+echo "Updating working copy..." && \
+git pull origin master && \
+echo "Committing changes..." && \
 git add *.html css/* && \
 git commit -m "$1" && \
+echo "Pushing changes from working copy to git repo..." && \
 git push && \
-scp -i /c/Users/Tiffany/.ssh/ec2.pem -r evidence ec2-user@tiffanyh.info:/var/www/vhosts/tiffanyh.info/htdocs/e-portfolio/ && \
+echo "Pulling changes from git repo to tiffanyh.info server..." && \
 ssh -i /c/Users/Tiffany/.ssh/ec2.pem ec2-user@tiffanyh.info 'cd /var/www/vhosts/tiffanyh.info/htdocs/e-portfolio/ && git pull'
